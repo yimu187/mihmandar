9f607ba8056bfa0cf0f1ff569a403a688e8bdf20
yimu187 <yimu187@gmail.com>
2016-11-18 15:18:04 +0200

    second commit

---
+++ MihmandarUi/src/main/webapp/VAADIN/widgetsets/com.vaadin.MihmandarWidgetset/deferredjs/1B269912B582869B307B33F1759F5FFD/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_vaadin_MihmandarWidgetset.runAsyncCallback2('Meb(1672,1,N8d);_.vc=function pjc(){o3b((!h3b&&(h3b=new t3b),h3b),this.a.d)};l2d(Uh)(2);\n//# sourceURL=com.vaadin.MihmandarWidgetset-2.js\n')
